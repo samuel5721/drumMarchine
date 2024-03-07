11bdea94d690f9ea8b0255d1d03583b6dd399156
samuel5721 <samuel20070731@gmail.com>
2024-03-07 17:39:31 +0900

    feat: drum marchine

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect, useRef } from 'react';
-import drum_kick from './assets/sound/drum_kick.wav';
-import drum_snare from './assets/sound/drum_snare.wav';
-import drum_hiHat from './assets/sound/drum_hiHat.wav';
+import kick from './assets/sound/drum_kick.wav';
+import snare from './assets/sound/drum_snare.wav';
+import hiHat from './assets/sound/drum_hiHat.wav';
+import cowBell from './assets/sound/drum_cowBell.mp3'
 
 import styles from './App.module.css';
 
@@ -9,25 +10,33 @@ function App() {
   const [audioContext, setAudioContext] = useState(null);
   const [isFetched, setIsFetched] = useState(false);
   const [isPlaying, setIsPlaying] = useState(false);
-  const [bpm, setBpm] = useState(120);
+
+  const [currentNote, setCurrentNote] = useState(0);
+  const bpm = useRef(120);
   const [intervalId, setIntervalId] = useState(null);
   const noteNum = 16;
-  const audioBuffer = useRef({
-    kick: null,
-    snare: null,
-    hiHat: null,
-  });
-  const soundMap = {
-    kick: drum_kick,
-    snare: drum_snare,
-    hiHat: drum_hiHat,
-  };
 
-  const [score, setScore] = useState({
-    kick: Array.from({length: noteNum}, () => false),
-    snare: Array.from({length: noteNum}, () => false),
-    hiHat: Array.from({length: noteNum}, () => false),
-  });
+  const soundMap = {
+    kick:kick,
+    snare:snare,
+    hiHat:hiHat,
+    cowBell:cowBell
+  };
+  const audioBuffer = useRef(
+    Object.keys(soundMap).reduce((acc, key) => ({ ...acc, [key]: null }), {})
+  );
+
+  const score = useRef(
+    Array.from({ length: 16 }, () => (
+      Object.keys(soundMap).reduce((acc, key) => ({
+        ...acc,
+        [key]: Array.from({ length: noteNum }, () => false)
+      }), {})
+    ))
+  );
+  const [seeingScore, setSeeingScore] = useState(score.current);
+  const currentSet = useRef(0);
+  const [seeingCurrentSet, setSeeingCurrentSet] = useState(currentSet.current );
 
   //오디오 초기화
   const initializeAudio = async () => {
@@ -67,34 +76,51 @@ function App() {
   };
 
   useEffect(() => {
-    if (isPlaying) {
-      let currentNote = 0;
+    clearInterval(intervalId);
 
-      const interval = setInterval(() => {
-        for (const ins in score) {
-            if (score[ins][currentNote]) playSound(ins);
+    if(!isPlaying) return;
+
+    let noteBuffer = 0;
+    let id = setInterval(() => {
+      for (const ins in score.current[currentSet.current]) {
+            if (score.current[currentSet.current][ins][noteBuffer]) playSound(ins);
         }
+        noteBuffer = (noteBuffer + 1) % 16;
+        setCurrentNote(currentNote => noteBuffer);
+    }, 1000/(bpm.current/60*4));
+    
+    setIntervalId(id);
 
-        currentNote = (currentNote + 1) % noteNum;
-      }, 1000/(bpm/60*4));
-
-      setIntervalId(interval);
-    } else {
-      if (intervalId) clearInterval(intervalId); 
+    return () => {
+      clearInterval(id);
+      setCurrentNote(0);
     }
 
-    return () => { 
-        if (intervalId) clearInterval(intervalId);
-    };
-  }, [isPlaying, bpm])
+  }, [isPlaying])
+
 
   //해당 악기와 줄에 해당하는 음표 변경
-  const changeScore = (ins, row) => {
-    const updatedScore = { ...score };
-    updatedScore[ins] = [...updatedScore[ins]];
-    updatedScore[ins][row] = !updatedScore[ins][row];
-    setScore(updatedScore);
+  const changeScore = (set, ins, row) => {
+    const updatedScore = score.current.map((scoreSet, index) => {
+      if (index === set) {
+        return {
+          ...scoreSet,
+          [ins]: scoreSet[ins].map((val, rowIndex) => rowIndex === row ? !val : val),
+        };
+      } else {
+        return scoreSet;
+      }
+    });
+  
+    score.current = updatedScore;
+    setSeeingScore([...updatedScore]);
   };
+
+  //악보 변경
+  const changeCurrentSet = (set) => {
+    currentSet.current = set;
+    setSeeingCurrentSet(currentSet.current);
+  }
 
   //play 버튼 함수
   const play = () => {
@@ -103,66 +129,98 @@ function App() {
 
   const stop = () => {
     setIsPlaying(false);
+    clearInterval(intervalId);
   }
 
   const scoreClear = () => {
-    const newScore = {};
-    for (const instrument in score) {
-        newScore[instrument] = Array.from({ length: noteNum }, () => false);
-    }
-    setScore(newScore);
-    setIsPlaying(false);
-  }
+    // score.current 전체를 복사합니다.
+    const newScore = score.current.map((scoreSet, index) => {
+        if (index === currentSet.current) {
+            const clearedSet = {};
+            for (const instrument in scoreSet) {
+                clearedSet[instrument] = Array.from({ length: noteNum }, () => false);
+            }
+            return clearedSet;
+        } else {
+            return scoreSet;
+        }
+    });
+
+    score.current = newScore;
+    setSeeingScore([...newScore]);
+}
+
 
   const changeBpm = (event) => {
     if(event.target.value <= 0) {
-      setBpm(1); return;
+      bpm.current = 1; return;
     }
     if(event.target.value > 1000) {
-      setBpm(999); return;
+      bpm.current = 999; return;
     }
-    setBpm(event.target.value);
+    bpm.current = event.target.value;
   }
 
   return (
-    <div>
+    <div className={styles.wrapper}>
       <h1>Press keys to play drums</h1>
       <div className={styles.optionWrapper}> 
-        <input type='number' value={bpm} onChange={changeBpm} onClick={(event) => {event.target.select();}}/>
-        <button onClick={initializeAudio}>Start Audio</button>
+        <input className={styles.optionInput} type='number' value={bpm.current} onChange={changeBpm} onClick={(event) => {event.target.select();}}/>
+        <button className={styles.optionBtn} onClick={initializeAudio}>Start</button>
         {
-          isPlaying ? <button onClick={stop}>stop</button>
-          :<button onClick={play}>Play</button>
+          isPlaying ?
+          <button className={styles.optionBtn} onClick={stop}>Stop</button> :
+          <button className={styles.optionBtn} onClick={play}>Play</button>
         }
-        <button onClick={scoreClear}>clear</button>
+        <button className={styles.optionBtn} onClick={scoreClear}>Clear</button>
       </div>
       
       <div className={styles.lineWrapper}>
         {
-          ['hiHat', 'snare', 'kick'].map((ins) => {
+          ['cowBell', 'hiHat', 'snare', 'kick', ].map((ins) => {
             return (
+              <>
+              <span>{ins}</span>
               <div className={styles.rowWrapper}>
                 {Array.from({ length: noteNum }).map((_, i) => {
                   return(
                     <button
                       className={styles.noteBtn}
-                      style={{ background:(score[ins][i]) ? '#72ac51' : (i%4 === 0) ? '#d6d6d6' : '#ebebeb' }}
+                      style={{ background:(seeingScore[seeingCurrentSet][ins][i]) ? 'black' : (i%4 === 0) ? '#eeeeee' : 'white' }}
                       onClick={()=>{
-                        if(!isPlaying) {
-                          if(!score[ins][i]) playSound(ins);
-                          changeScore(ins, i);
-                        }
+                        changeScore(currentSet.current, ins, i);
                     }}>
-                      {(i%4 === 0 && !score[ins][i]) ? (i+4)/4 : ''}
                     </button>
                   )
                 })}
               </div>
+              </>
+              
             );
           })
         }
+        <br />
+        {
+          <div className={styles.rowWrapper}>
+          {Array.from({ length: noteNum }).map((_, i) => {
+            return(
+              <button
+                className={styles.noteBtn}
+                style={{
+                  background: ((currentNote === i && isPlaying) || seeingCurrentSet === i) ? 'black' : (i%4 === 0) ? '#eeeeee' : 'white',
+                  color: ((currentNote === i && isPlaying) || seeingCurrentSet === i) ? 'white' : 'black'
+                }}
+                onClick={()=>{
+                  changeCurrentSet(i); 
+              }}>
+                {i+1}
+              </button>
+            )
+          })}
+        </div>
+        }
       </div>
-      <button onClick={ () => { console.log(score); } }>check</button>
+      <button onClick={ () => { console.log(score.current); } }>check</button>
     </div>
   );
 }
